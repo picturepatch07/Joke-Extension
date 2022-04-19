a74436513bb171945650f09b49d5dcc8adc38d16
Abhishek Anand <anand2000ys@gmail.com>
2022-04-19 10:27:48 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch("https://icanhazdadjoke.com/slack").then(data => data.json()).then(jokeData => {
+    const jokeText = jokeData.attachments[0].text;
+    const jokeElement = document.getElementById('jokeElement');
+
+    jokeElement.innerHTML = jokeText;
+})
